9e36635551285da711c766a3d2e25061e05e3386
Anuj <anujjhace@gmail.com git config --global user.name anuj>
2024-12-03 16:43:40 +0530

    Initial Commit

---
+++ Assignemt/AssignemtApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignemtApp.swift
+//  Assignemt
+//
+//  Created by ANUJ KUMAR on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AssignemtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
